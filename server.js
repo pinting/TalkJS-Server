f33975538a6d7b56ac8841549b574b64d31be5cf
Tornyi Dénes <tornyi.denes@gmail.com>
2014-04-13 20:49:59 +0200

    Database init

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 new require("./lib/server")({
     ip: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
     port: process.env.OPENSHIFT_NODEJS_PORT || 8000,
-    database: "./talk.db"
+    database: "./users.db"
 });
